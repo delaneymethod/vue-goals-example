b551a6bcc025c2ca84d4e14a81c111b0775acbbc
Sean Delaney <hello@delaneymethod.com>
2021-04-19 18:27:43 +0100

    Linting
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <div>
-    <header>
-      <h1>Vue Course Goals</h1>
-    </header>
-    <section id="user-goal">
-      <h2>My Course Goal</h2>
-      <p>{{ courseGoal }}</p>
-      <p>Learn more <a v-bind:href="courseLink">about Vue</a></p>
-    </section>
-  </div>
+	<div>
+		<header>
+			<h1>Vue Course Goals</h1>
+		</header>
+		<section id="user-goal">
+			<h2>My Course Goal</h2>
+			<p>{{ outputGoal() }}</p>
+			<p>Learn more <a v-bind:href="courseLink">about Vue</a></p>
+		</section>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {},
-  data() {
-    return {
-      courseGoal: 'Master Vue!',
-      courseLink: 'https://vuejs.org/'
-    };
-  }
+	name: 'App',
+	components: {},
+	data: () => ({
+		courseGoalA: 'Master Vue!',
+		courseGoalB: 'Learn Vue!',
+		courseLink: 'https://vuejs.org/'
+	}),
+	methods: {
+		outputGoal() {
+			const randomNumber = Math.random();
+
+			if (randomNumber < 0.5) {
+				return this.courseGoalA;
+			} else {
+				return this.courseGoalB;
+			}
+		}
+	}
 };
 </script>
 
