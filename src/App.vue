2996ec76e6e502bbf571cda78e29a57893320103
Sean Delaney <hello@delaneymethod.com>
2021-04-19 18:04:58 +0100

    More project cleanup
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
-	<div>
-		<header>
-			<h1>Vue Course Goals</h1>
-		</header>
-		<section id="user-goal">
-			<h2>My Course Goal</h2>
-			<p>{{ courseGoal }}</p>
-		</section>
-	</div>
+  <div>
+    <header>
+      <h1>Vue Course Goals</h1>
+    </header>
+    <section id="user-goal">
+      <h2>My Course Goal</h2>
+      <p>{{ courseGoal }}</p>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'App',
-	components: {},
-	data() {
-		return {
-			courseGoal: 'Master Vue!'
-		};
-	}
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      courseGoal: 'Master Vue!'
+    };
+  }
 };
 </script>
 
