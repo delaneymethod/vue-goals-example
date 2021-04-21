72be182d17bee95e2260f4753755f0d1642956b2
Sean Delaney <hello@delaneymethod.com>
2021-04-21 14:14:35 +0100

    Dynamic components example
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

---
+++ src/components/Badges/ActiveGoals.vue
@@ -0,0 +1,11 @@
+<template>
+	<h2>Active Goals</h2>
+</template>
+
+<script>
+export default {
+	name: 'ActiveGoals'
+};
+</script>
+
+<style scoped></style>
--- src/components/Badges/Badges.vue
+++ src/components/Badges/Badges.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
 		<TheHeader />
+		<!--
 		<BadgeList />
 		<UserInfo
 			:full-name="activeUser.name"
@@ -11,31 +12,54 @@
 			<h2>{{ slotProps.item }}</h2>
 			<p>{{ slotProps['anotherItem'] }}</p>
 		</CourseGoals>
+		-->
+		<button @click="setSelectedComponent('active-goals')">Active Goals</button>
+		<button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+		<!--
+		<ActiveGoals v-if="selectedComponent === 'active-goals'" />
+		<ManageGoals v-if="selectedComponent === 'manage-goals'" />
+		-->
+		<component :is="selectedComponent" />
 	</div>
 </template>
 
 <script>
+import ActiveGoals from '@/components/Badges/ActiveGoals';
+import ManageGoals from '@/components/Badges/ManageGoals';
 import TheHeader from '@/components/Badges/TheHeader';
+/*
 import BadgeList from '@/components/Badges/BadgeList';
 import UserInfo from '@/components/Badges/UserInfo';
 import CourseGoals from '@/components/Badges/CourseGoals';
+*/
 
 export default {
 	name: 'Badges',
 	components: {
+		ManageGoals,
+		ActiveGoals,
+		TheHeader
+		/*,
 		CourseGoals,
-		TheHeader,
+
 		BadgeList,
 		UserInfo
+		*/
 	},
 	data() {
 		return {
+			selectedComponent: 'active-goals',
 			activeUser: {
 				name: 'Sean Delaney',
 				description: 'Site owner and admin',
 				role: 'admin'
 			}
 		};
+	},
+	methods: {
+		setSelectedComponent(component) {
+			this.selectedComponent = component;
+		}
 	}
 };
 </script>
