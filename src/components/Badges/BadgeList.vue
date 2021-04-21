d9c0c73304ccba145f0d1fc6fc9f3ee64ab88065
Sean Delaney <hello@delaneymethod.com>
2021-04-21 13:21:16 +0100

    More tweaks
    Signed-off-by: Sean Delaney <hello@delaneymethod.com>

--- src/components/Badges/BadgeList.vue
+++ src/components/Badges/BadgeList.vue
@@ -25,6 +25,12 @@ export default {
 
 <style lang="scss" scoped>
 section {
+	margin: 2rem auto;
+	max-width: 30rem;
+	border-radius: 12px;
+	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+	padding: 1rem;
+
 	h2 {
 		margin: 0.5rem 0;
 		color: #3a3a3a;
